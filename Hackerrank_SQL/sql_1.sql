b314f8537d5d66f1c09422f159c80f7e2e2df35b
allen <allenareeckal@gmail.com>
2022-09-08 23:19:51 +0530

    FOURTH COMMIT

--- Hackerrank_SQL/sql_1.sql
+++ Hackerrank_SQL/sql_1.sql
@@ -55,8 +55,41 @@ WHERE SUBSTRING(CITY,1,1) IN ('A','E','I','O','U')
 SELECT DISTINCT CITY FROM STATION
 WHERE SUBSTRING(REVERSE(CITY),1,1) IN ('A','E','I','O','U');
 
+--SQL 13
+--Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION
+WHERE
+SUBSTRING(REVERSE(CITY),1,1) IN ('A','E','I','O','U')
+and SUBSTRING(CITY,1,1) IN ('A','E','I','O','U');
+
+--SQL 14
+--Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION
+WHERE SUBSTRING(CITY,1,1) NOT IN ('A','E','I','O','U');
+
+--SQL 15
+--Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION
+WHERE SUBSTRING(REVERSE(CITY),1,1) NOT IN ('A','E','I','O','U');
+
+--SQL 16
+--Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION
+WHERE
+SUBSTRING(REVERSE(CITY),1,1) NOT IN ('A','E','I','O','U','a','e','i','o','u')
+OR SUBSTRING(CITY,1,1) NOT IN ('A','E','I','O','U','a','e','i','o','u');
 
 
+--SQL 17
+--Query the list of CITY names from STATION that either do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION
+WHERE
+SUBSTRING(REVERSE(CITY),1,1) NOT IN ('A','E','I','O','U','a','e','i','o','u')
+and SUBSTRING(CITY,1,1) NOT IN ('A','E','I','O','U','a','e','i','o','u');
 
-
+--SQL 18
+--Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+SELECT NAME FROM STUDENTS
+WHERE MARKS >75
+ORDER BY  REVERSE(SUBSTRING(REVERSE(NAME),1,3)),ID
 
